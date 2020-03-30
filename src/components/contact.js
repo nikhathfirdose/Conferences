2ab40344cbb82ad5061531663a81e7d1efc5b7a0
nikhathfirdose <nikhathfirdose@github.com>
2020-03-30 19:35:10 +0530

    Required data added

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,9 +9,17 @@ const Contacts = ({ contacts }) => {
       {contacts.map(contact => (
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">{contact.confStartDate}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{contact.city}</h6>
-            <p class="card-text">{contact.country}</p>
+            <img class="image" src={contact.imageURL} alt="Poster Image" />
+            <h5 class="conf-date">Date: {contact.confStartDate}</h5>
+            <h6 class="conf-name">Conference Name: {contact.confName}</h6>
+            <p class="conf-entry">{contact.entryType}</p>
+            <p class="location">
+              {" "}
+              Location: {contact.city}, {contact.country}
+            </p>
+            <a class="details" href={contact.confUrl} target="_blank">
+              Read More{" "}
+            </a>
           </div>
         </div>
       ))}
