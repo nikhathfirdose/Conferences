8535b7f1fca7f4229f94ab3a0242bfc3d5fcb2d3
nikhathfirdose <nikhathfirdose@github.com>
2020-03-30 18:58:25 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
-import Header from "./components/header";
-import MainContent from "./components/content";
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
+import React, { Component } from "react";
+import Contacts from "./components/contact";
 
+class App extends Component {
+  state = {
+    contacts: []
+  };
+  componentDidMount() {
+    fetch(
+      "https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences"
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ contacts: data.paid });
+      })
+      .catch(console.log);
+  }
+  render() {
+    return (
+      <div>
+        <Contacts contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
 export default App;
